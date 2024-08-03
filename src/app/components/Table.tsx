f23c0222bc242cb96880919079af51fd2401e3fb
gejaga12 <gejaga12@hotmail.com>
2024-08-02 22:28:15 -0300

    Add: agrego pagina 2.

--- src/app/components/Table.tsx
+++ src/app/components/Table.tsx
@@ -1,14 +1,15 @@
 "use client"
 import React, { useState } from 'react';
 import { TableData } from '../../types';
+import { FaChevronDown, FaChevronUp } from 'react-icons/fa';
 
 interface TableProps {
   title: string;
   data: TableData[];
 }
 
-const Table: React.FC<TableProps> = ({ title, data }) => {
-  const [isExpanded, setIsExpanded] = useState(false);
+const Table: React.FC<TableProps> = ({ title, data = [] }) => {  // Proporcionar un valor predeterminado vacío para data
+  const [isExpanded, setIsExpanded] = useState(true);
 
   const toggleExpand = () => {
     setIsExpanded(!isExpanded);
@@ -16,31 +17,42 @@ const Table: React.FC<TableProps> = ({ title, data }) => {
 
   return (
     <div className="overflow-x-auto mb-4">
-      <h2 
-        className="text-lg font-semibold my-2 bg-slate-500 text-white p-2 cursor-pointer"
+      <div 
+        className="flex items-center justify-between text-lg font-semibold my-2 bg-slate-600 text-white p-2 cursor-pointer rounded-ee-lg rounded-ss-lg"
         onClick={toggleExpand}
       >
-        {title}
-      </h2>
-      {isExpanded && (
-        <table className="min-w-full bg-white dark:bg-gray-800 text-sm">
+        <h2>{title}</h2>
+        <span className="ml-2">
+          {isExpanded ? <FaChevronUp /> : <FaChevronDown />}
+        </span>
+      </div>
+      {isExpanded && data && data.length > 0 && (  // Añadir comprobación para verificar que data no es undefined y tiene elementos
+        <table className="min-w-full bg-white dark:bg-gray-800 text-xs">
           <thead>
             <tr>
-              <th className="py-1 px-2 text-black dark:text-white text-start">Item</th>
-              <th className="py-1 px-2 text-black dark:text-white text-start">Unidad</th>
-              <th className="py-1 px-2 text-black dark:text-white text-start">Costo</th>
+              <th className="py-2 px-4 text-black dark:text-white text-start">ITEM</th>
+              <th className="py-2 px-4 text-black dark:text-white text-start">UNIDAD</th>
+              <th className="py-2 px-4 text-black dark:text-white text-start">COSTO</th>
             </tr>
           </thead>
           <tbody>
             {data.map((row, index) => (
-              <tr key={index}>
-                <td className="border px-2 py-1 text-black dark:text-white hover:bg-slate-100">{row.item}</td>
-                <td className="border px-2 py-1 text-black dark:text-white hover:bg-slate-100">{row.unidad}</td>
-                <td className="border px-2 py-1 text-black dark:text-white hover:bg-slate-100">${row.costo}</td>
+              <tr 
+                key={index} 
+                className="even:bg-gray-100 odd:bg-white dark:even:bg-gray-700 dark:odd:bg-gray-800 hover:bg-slate-100 dark:hover:bg-slate-700"
+              >
+                <td className="border px-4 py-2 text-black dark:text-white">{row.item}</td>
+                <td className="border px-4 py-2 text-black dark:text-white">{row.unidad}</td>
+                <td className="border px-4 py-2 text-black dark:text-white">${row.costo}</td>
               </tr>
             ))}
           </tbody>
         </table>
+      )}
+      {isExpanded && (!data || data.length === 0) && (
+        <div className="text-center text-gray-500 dark:text-gray-400 p-4">
+          No data available
+        </div>
       )}
     </div>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,51 @@
-"use client"
 import Navbar from './components/Navbar';
 import Table from './components/Table';
 import ReferencesDashboard from './components/ReferencesDashboard';
 import data from './datos/data.json';
+import { TableData } from '../types';
 
-export default function Home() {
+const Home: React.FC = () => {
+  const {
+    estudioEnsayoSuelo,
+    pagosComun,
+    andeDerechos,
+    essapDerechos,
+    trabajos,
+    demonteDemoliciones,
+    estructurasMetalicasConTechoDeChapa,
+    estructurasPremoldeadas,
+    estructurasDeMadera,
+    estructurasDeAlbanileria,
+    albanileriaDeLadrillos,
+    aislacionVertical,
+    aislacionDeBanos,
+    aislacionTermica,
+    techosDeFibrocemento,
+  }: any = data;
+
   return (
     <div className="bg-gray-100 dark:bg-gray-900 min-h-screen">
       <Navbar />
       <div className="container mx-auto p-4">
         <ReferencesDashboard />
-        <Table title="Estudio y Ensayo de Suelo" data={data.estudioEnsayoSuelo} />
-        <Table title="Pagos del Comunero - Derecho de Construcción" data={data.pagosComún} />
-        <Table title="Ande - Derechos" data={data.andeDerechos} />
-        <Table title="Essap - Derechos" data={data.essapDerechos} />
-        <Table title="Trabajos" data={data.trabajos} />
-        <Table title="Demonte y Demoliciones" data={data.demonteDemoliciones} />
+        <Table title="Estudio y Ensayo de Suelo" data={estudioEnsayoSuelo as TableData[]} />
+        <Table title="Pagos del Comunero - Derecho de Construcción" data={pagosComun as TableData[]} />
+        <Table title="Ande - Derechos" data={andeDerechos as TableData[]} />
+        <Table title="Essap - Derechos" data={essapDerechos as TableData[]} />
+        <Table title="Trabajos" data={trabajos as TableData[]} />
+        <Table title="Demonte y Demoliciones" data={demonteDemoliciones as TableData[]} />
+        <Table title="Estructuras Metálicas con Techo de Chapa" data={estructurasMetalicasConTechoDeChapa as TableData[]} />
+        <Table title="Estructuras Premoldeadas" data={estructurasPremoldeadas as TableData[]} />
+        <Table title="Estructuras de Madera" data={estructurasDeMadera as TableData[]} />
+        <Table title="Estructuras de Albañilería" data={estructurasDeAlbanileria as TableData[]} />
+        <Table title="Albañilería de Ladrillos" data={albanileriaDeLadrillos as TableData[]} />
+        <Table title="Aislación Vertical" data={aislacionVertical as TableData[]} />
+        <Table title="Aislación de Baños" data={aislacionDeBanos as TableData[]} />
+        <Table title="Aislación Térmica" data={aislacionTermica as TableData[]} />
+        <Table title="Techos de Fibrocemento" data={techosDeFibrocemento as TableData[]} />
       </div>
     </div>
   );
-}
+};
+
+export default Home;
