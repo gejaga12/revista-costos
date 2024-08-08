edfedd12476b857c64547cec67899da3b0b545a5
gejaga12 <gejaga12@hotmail.com>
2024-08-08 12:21:55 -0300

    Add: pagina en consturccion para historial

---
+++ src/app/historial/page.tsx
@@ -0,0 +1,15 @@
+// historial page
+
+import React from 'react';
+import { BsTools } from 'react-icons/bs';
+
+const HistorialPage: React.FC = () => {
+    return (
+        <div className='flex flex-col justify-center items-center h-screen bg-black'>
+            <BsTools className='text-white pb-5'  size={150}/>                
+            <h1 className='text-3xl font-light text-white'>Pagina en construcción</h1>
+        </div>
+    );
+};
+
+export default HistorialPage;
