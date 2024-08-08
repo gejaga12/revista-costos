4e4fa4e5c366e96028b61200801a77550efa45eb
gejaga12 <gejaga12@hotmail.com>
2024-08-08 15:20:32 -0300

    Fix: arreglo parpadeo de navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 import { Disclosure, Menu, Transition } from '@headlessui/react';
 import { Bars3Icon, BellIcon, XMarkIcon, UserCircleIcon } from '@heroicons/react/24/outline';
 import { FaSun, FaMoon } from 'react-icons/fa';
@@ -35,7 +36,7 @@ const Navbar: React.FC = () => {
           <div className="mx-auto px-2 sm:px-6 lg:px-8">
             <div className="relative flex h-24 items-center justify-between"> {/* Aumentar la altura */}
               <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                {/* Mobile menu button*/}
+                {/* Mobile menu button */}
                 <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
                   <span className="sr-only">Open main menu</span>
                   {open ? (
@@ -47,25 +48,25 @@ const Navbar: React.FC = () => {
               </div>
               <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-between">
                 <div className="flex flex-shrink-0 items-center">
-                  <a href="/">
-                    <Image src="/logo.webp" alt="Logo" width={350} height={280} className="block h-16 w-auto" /> {/* Aumentar el tamaño del logo */}
-                  </a>
+                  <Link href="/">
+                
+                      <Image src="/logo.webp" alt="Logo" width={350} height={280} className="block h-16 w-auto" /> {/* Aumentar el tamaño del logo */}
+                 
+                  </Link>
                 </div>
                 {user && ( // Mostrar solo si el usuario está autenticado
                   <div className="hidden sm:flex sm:ml-auto sm:mr-auto items-center justify-center"> {/* Flex contenedor centrado */}
                     <div className="flex space-x-4">
                       {navigation.map((item) => (
-                        <a
-                          key={item.name}
-                          href={item.href}
-                          className={classNames(
-                            item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                            'rounded-md px-3 py-2 text-sm font-medium'
-                          )}
-                          aria-current={item.current ? 'page' : undefined}
-                        >
-                          {item.name}
-                        </a>
+                        <Link key={item.name} href={item.href}  className={classNames(
+                          item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'rounded-md px-3 py-2 text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >                        
+                            {item.name}
+                        
+                        </Link>
                       ))}
                     </div>
                   </div>
@@ -101,32 +102,29 @@ const Navbar: React.FC = () => {
                       <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="/"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Inicio
-                            </a>
+                            <Link href="/"className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                             
+                                Inicio
+                             
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="/historial"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Historial de Presupuestos
-                            </a>
+                            <Link href="/historial" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                              
+                                Historial de Presupuestos
+                             
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
                           {({ active }) => (
-                            <a
-                              href="/perfil"
-                              className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}
-                            >
-                              Perfil
-                            </a>
+                            <Link href="/perfil" className={classNames(active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700')}>
+                              
+                                Perfil
+                           
+                            </Link>
                           )}
                         </Menu.Item>
                         <Menu.Item>
@@ -144,9 +142,9 @@ const Navbar: React.FC = () => {
                   </Menu>
                 </div>
               )}
-                  <button onClick={toggleTheme} className="text-white ml-4">
-                    {theme === 'light' ? <FaMoon className="h-6 w-6" /> : <FaSun className="h-6 w-6" />}
-                  </button>
+              <button onClick={toggleTheme} className="text-white ml-4">
+                {theme === 'light' ? <FaMoon className="h-6 w-6" /> : <FaSun className="h-6 w-6" />}
+              </button>
             </div>
           </div>
 
