e5beef4672dc472e192a6c7b451fd1c36c0935c7
gejaga12 <gejaga12@hotmail.com>
2024-08-08 20:15:36 -0300

    Add: arreglo vista de perfil

--- src/app/components/perfil/ProfileDetails.tsx
+++ src/app/components/perfil/ProfileDetails.tsx
@@ -1,6 +1,6 @@
-// ProfileDetails.tsx
 import React from 'react';
 import { useAuth } from '../../context/AuthContext';
+import { PhoneIcon, UserIcon, EnvelopeIcon, MapPinIcon, BuildingOffice2Icon } from '@heroicons/react/24/outline';
 
 const ProfileDetails: React.FC = () => {
   const { user } = useAuth();
@@ -10,31 +10,62 @@ const ProfileDetails: React.FC = () => {
   }
 
   return (
-    <div className="mt-6">
-    <dl className="grid grid-cols-1 gap-x-2 gap-y-8 sm:grid-cols-2">        
-      <div className="sm:col-span-1">
-        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300">Nombre de Usuario</dt>
-        <dd className="mt-1 text-sm text-gray-900 dark:text-white">{user.username}</dd>
-      </div>
-      <div className="sm:col-span-1">
-        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300">Correo Electrónico</dt>
-        <dd className="mt-1 text-sm text-gray-900 dark:text-white">{user.email || 'No proporcionado'}</dd>
-      </div>
-      <div className="sm:col-span-1">
-        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300">Teléfono</dt>
-        <dd className="mt-1 text-sm text-gray-900 dark:text-white">
-          {user.phone || 'No proporcionado'}
-        </dd>
-      </div>
-      <div className="sm:col-span-1">
-        <dt className="text-sm font-medium text-gray-500 dark:text-gray-300">Dirección</dt>
-        <dd className="mt-1 text-sm text-gray-900 dark:text-white">
-          {user.address || 'No proporcionado'}
-        </dd>
-      </div>
-    </dl>
-    
-  </div>
+    <div className="mt-2">
+      <dl className="grid grid-cols-1 gap-x-4 gap-y-4 sm:grid-cols-2 lg:grid-cols-3">
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <UserIcon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">Nombre de Usuario</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">{user.username}</dd>
+          </div>
+        </div>
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <EnvelopeIcon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">Correo Electrónico</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">{user.email}</dd>
+          </div>
+        </div>
+
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <PhoneIcon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">Teléfono</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">{user.phone || "123456789"}</dd>
+          </div>
+        </div>
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <MapPinIcon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">Dirección</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">{user.address || "Av. Avellaneda 1547"}</dd>
+          </div>
+        </div>
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <MapPinIcon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">RUT</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">3772454856</dd>
+          </div>
+        </div>
+
+        <div className="sm:col-span-1 flex items-center bg-gray-100 p-2 rounded-lg shadow-md">
+          <BuildingOffice2Icon className="h-5 w-5 text-red-600 border-red-600 dark:text-gray-300 mr-2" />
+          <div className='dark:bg-gray-700 border-gray-500 border-s-2 ps-2'>
+            <dt className="text-xs font-medium text-gray-500 dark:text-gray-300">Empresa</dt>
+            <dd className="mt-1 text-xs text-gray-900 dark:text-white">HOGAR SERMAN SRL</dd>
+          </div>
+        </div>
+
+
+
+      </dl>
+    </div>
   );
 };
 
--- src/app/perfil/page.tsx
+++ src/app/perfil/page.tsx
@@ -1,49 +1,38 @@
-// ProfilePage.tsx
 "use client";
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
 import ProfileDetails from '../components/perfil/ProfileDetails';
-import { UserCircleIcon } from '@heroicons/react/24/outline';
+import { BriefcaseIcon, StarIcon } from '@heroicons/react/24/solid';
+import Image from 'next/image';
 
 const ProfilePage: React.FC = () => {
-    const { user } = useAuth();
 
-    const PresupuestosCard: React.FC = () => {
-        return (
-            <div className="bg-gray-200 text-white rounded-lg shadow-lg p-4 space-y-1 flex flex-col items-center w-full sm:w-auto">
-                <h2 className="text-sm text-black font-bold">Presupuestos</h2>
-                <p className="text-xs text-white bg-black rounded-lg p-1 text-center w-10">10</p>
-            </div>
-        );
-    };
-
-    const MembresiaCard: React.FC = () => {
-        return (
-            <div className="bg-gray-200 text-white rounded-lg shadow-lg p-4 space-y-1 flex flex-col items-center w-full sm:w-auto">
-                <h2 className="text-sm text-black font-bold">Membresía</h2>
-                <p className="text-xs text-yellow-600 bg-black rounded-lg p-1 px-2 text-center">Premium</p>
-            </div>
-        );
-    };
+    const { user } = useAuth(); 
 
     return (
-        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 p-6">
-            <div className="max-w-2xl mx-auto bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6">
-                <div className="flex flex-col sm:flex-row items-center justify-between space-y-4 sm:space-y-0">
-                    <div className="flex flex-col sm:flex-row items-center justify-center space-y-2 sm:space-y-0 sm:space-x-2">
-                        <UserCircleIcon className="h-16 w-16 text-gray-500 dark:text-gray-300 mr-2" />
-                        <h1 className="text-2xl font-bold text-gray-900 dark:text-white">Perfil de Usuario</h1>
-                    </div>
-                    <div className="flex flex-row sm:flex-row justify-center items-center space-y-2 sm:space-y-0 sm:space-x-6 space-x-3">
-                        <PresupuestosCard />
-                        <MembresiaCard />
+        <div className="min-h-screen bg-gray-100 dark:bg-gray-900 p-6 items-center flex">
+            <div className="max-w-5xl mx-auto bg-white dark:bg-gray-800 rounded-lg shadow-lg p-6 flex flex-col sm:flex-row items-center space-y-4 sm:space-y-0 sm:space-x-4">
+                <div className="relative flex-shrink-0 mr-2">
+                    <Image
+                        src="/foto-perfil.png"
+                        alt="Profile"
+                        width={150}
+                        height={150}
+                        className="rounded-lg object-cover"
+                    />
+                    <div className="absolute bottom-1 left-1 text-black text-[10px] font-semibold py-0.5 px-1 rounded">
+                    {/* <StarIcon className="h-5 w-5 text-yellow-400 " /> */}
                     </div>
                 </div>
-                {user ? (
-                    <ProfileDetails />
-                ) : (
-                    <p className="mt-4 text-gray-600 dark:text-gray-400">No se encontró información del usuario.</p>
-                )}
+                <div className="flex-grow">
+                    <h1 className="text-lg font-semibold text-gray-900 dark:text-white">Perfil de Usuario</h1> 
+
+                    {user ? (
+                        <ProfileDetails />
+                    ) : (
+                        <p className="mt-4 text-gray-600 dark:text-gray-400">No se encontró información del usuario.</p>
+                    )}
+                </div>
             </div>
         </div>
     );
